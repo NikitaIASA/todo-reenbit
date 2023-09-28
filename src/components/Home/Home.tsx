b2f3c059aba53ff53d925ff98162919f49cd6962
Nikita Chaika <iasastudent2021@gmail.com>
2023-09-28 22:37:00 +0300

    feat(input field): added plus button

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,22 +1,30 @@
 import { FC } from "react";
-import { useSelector } from "react-redux"; 
+import { useSelector } from "react-redux";
 
 import Container from "../Container";
 import Header from "../Header";
 import TodoInput from "../TodoInput";
 import ToDoDashboard from "../TodoDashboard";
+import AddTodoButton from "../AddTodoButton";
 import { selectTodoItems } from "@/redux/selectors/todoSelectors";
 
 import "./Home.scss";
 
+function test() {
+  alert("hello world");
+}
+
 export const Home: FC = () => {
-  const todoItems = useSelector(selectTodoItems); 
+  const todoItems = useSelector(selectTodoItems);
 
   return (
     <Container>
       <Header />
       <main className="main">
-        <TodoInput />
+        <div className="input-container">
+          <TodoInput />
+          <AddTodoButton onAddTodoButtonClick={test} />
+        </div>
         <ToDoDashboard items={todoItems} />
       </main>
     </Container>
