34bd23272b0cc0928f636d815fa159dc4f92f624
Nikita Chaika <iasastudent2021@gmail.com>
2023-10-08 22:52:02 +0300

    feat(SearchInput): added clear icon

--- src/components/SearchInput/SeachInput.tsx
+++ src/components/SearchInput/SeachInput.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react";
+import { FC, useRef } from "react";
 
 import "./SearchInput.scss";
 
@@ -8,13 +8,37 @@ interface SearchInputProps {
 }
 
 export const SearchInput: FC<SearchInputProps> = ({ value, onChange }) => {
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onChange(e.target.value);
+  };
+
+  const onClear = () => {
+    onChange("");
+    inputRef.current?.focus();
+  };
+
   return (
-    <input
-      className="search-input"
-      type="text"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-      placeholder="Search..."
-    />
+    <div className="search">
+      <input
+        ref={inputRef}
+        className="search__input"
+        type="text"
+        value={value}
+        onChange={handleInputChange}
+        placeholder="Search..."
+      />
+      {value && (
+        <svg
+          onClick={onClear}
+          className="search__clear-icon"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path d="M10 8.586L2.929 1.515 1.515 2.929 8.586 10l-7.071 7.071 1.414 1.414L10 11.414l7.071 7.071 1.414-1.414L11.414 10l7.071-7.071-1.414-1.414L10 8.586z" />
+        </svg>
+      )}
+    </div>
   );
 };
